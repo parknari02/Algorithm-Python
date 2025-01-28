1a4126369226cb5e36c91113b8faf3596e3ed3d5
parknari02 <pnr021118@naver.com>
2025-01-28 22:22:37 +0900

    2164번 풀이

---
+++ 2164번.py
@@ -0,0 +1,14 @@
+from collections import deque
+n = int(input())
+
+myQueue = deque()
+
+for i in range(1, n+1):
+    myQueue.append(i)
+
+while len(myQueue) > 1:
+    myQueue.popleft()
+    myQueue.append(myQueue.popleft())
+
+
+print(myQueue[0])
