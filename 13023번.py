49b146dba345f5d39e9621e1daea3af57837cbe5
parknari02 <pnr021118@naver.com>
2025-01-29 20:17:44 +0900

    1260번 풀이

--- 13023번.py
+++ 13023번.py
@@ -1,5 +1,31 @@
 #친구 관계 파악하기
 N, M = map(int, input().split())
 A = [[]for _ in range(N+1)]
-visited = []
+visited = [False] * (N+1)
 arrive = False
+
+def DFS(now, depth):
+    global arrive
+    if (depth == 5):
+        arrive = True
+        return
+    visited[now] = True
+    for i in A[now]:
+        if not visited[i]:
+            DFS(i, depth+1)
+    visited[now] = False
+
+for _ in range(M):
+    s, e = map(int, input().split())
+    A[s].append(e)
+    A[e].append(s)
+
+for i in range(N):
+    DFS(i, 1)
+    if arrive:
+        break
+
+if arrive:
+    print(1)
+else:
+    print(0)
